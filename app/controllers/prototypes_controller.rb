05337f751b6af13048cdbb8acd71068eb41a242c
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-10-09 08:53:27 +0900

    deviceの設定コントローラー

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -7,7 +7,16 @@ class PrototypesController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    user.create(user_params)
+  end
   
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile,:occupation, :position, :password_confirmation)
+  end
 
   def move_to_index
     unless user_signed_in?
